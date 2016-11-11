f0885604add39b5cd272bdf70ab2086e495bbb7a
Miles J <gloveslam29@hanmail.net>
2016-11-11 15:14:07 +0900

    Update AppMain.java

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -5,9 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git");
         System.out.println("규민아 힘내!!!!");
-	
-
-        System.out.println("bug fix");
+	System.out.println("bug fix");
 
 		
 		
