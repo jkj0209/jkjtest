23fdf4c8fd507765fe32ab66e539f9b3b917e1e5
Miles J <gloveslam29@hanmail.net>
2016-11-11 14:49:46 +0900

    뀨뀨

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -3,10 +3,11 @@ package edu.java.git;
 public class AppMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world1");
+		System.out.println("Hello, Git");
 		System.out.println("¾È³çÇÏ¼¼¿ä~");
         System.out.println("규민아 힘내!!!!");
 	
+		
 	}
 
 }
