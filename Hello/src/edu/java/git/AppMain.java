dd6a91e36cd653819c50a43caf14e7653d6d43fd
user <user@user-pc>
2016-11-11 15:14:36 +0900

    버그수정

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -5,9 +5,9 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git");
         System.out.println("규민아 힘내!!!!");
-	
-
-        System.out.println("새로운 기능 추가");
+	    System.out.println("나도 버그 수정...");
+	    
+	    
 
 	}
 
